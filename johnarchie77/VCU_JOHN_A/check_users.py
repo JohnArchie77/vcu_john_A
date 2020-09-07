14f5d4dbe731ee8bb6e65f27036cd87230f4e203
JohnArchie77 <70184422+JohnArchie77@users.noreply.github.com>
2020-09-07 12:36:24 -0400

    edits

--- johnarchie77/VCU_JOHN_A/check_users.py
+++ johnarchie77/VCU_JOHN_A/check_users.py
@@ -1,20 +1,17 @@
 def check_users(current_us, new_us):
     pass
-    current_us = ['chris', 'haritha', 'sally', 'darnell', 'superman']
-    new_us = ['george', 'ringo', 'superman', 'hannibal']
-    new_list = []
-    for current_us in current_us:
-        new_list.append(current_us.lower())
+    current_us = ['chris', 'haritha', 'sally', 'darnell', 'superman','john']
+    new_us = ['george', 'ringo', 'superman', 'hannibal','John']
 
-available = [itm.lower() for itm in current_us]
-for new_us in new_us:
-    if new_us.lower() in current_us:
-        print("Sorry, can not use that user name")
-    else:
+    current_us = [itm.lower() for itm in current_us]
+    for new_us in new_us:
+        if new_us.lower() in current_us:
+            print("Sorry, can not use that user name")
+        else:
             print("That user name is available ")
 
 
 if __name__=="__main__":
-    current_us = ['chris', 'haritha', 'sally', 'darnell', 'superman']
-    new_us = ['george', 'ringo', 'superman', 'hannibal']
+    current_us = ['chris', 'haritha', 'sally', 'darnell', 'superman','john']
+    new_us = ['george', 'ringo', 'superman', 'hannibal','John']
     check_users(current_us, new_us)
