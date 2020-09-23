1ea980e3737d094b1561579002955507018f624f
JohnArchie77 <70184422+JohnArchie77@users.noreply.github.com>
2020-09-23 16:46:25 -0400

    Update adding_numbers.py
    homework week 5

--- adding_numbers.py
+++ adding_numbers.py
@@ -4,9 +4,9 @@ import sys
 def add_them_all(filename):
     sum = 0
     
-    filename = open(filename, 'r')
+    fname = open(filename, "r")
 
-    for line in filename:
+    for line in fname:
         sum = sum +int(line)
     
     return sum
@@ -15,4 +15,3 @@ if __name__ == "__main__":
     fname=sys.argv[1]
     print(f"Processing {fname}")
     print(add_them_all(fname))
-    
